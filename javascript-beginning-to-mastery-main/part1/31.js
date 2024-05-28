ec3ef106875164ccdad493bb1528366051481003
ABDUL-REHMAN <abdulrehman034551@gmail.com>
2024-05-28 14:51:49 +0500

    Add More Information about topic

--- javascript-beginning-to-mastery-main/part1/31.js
+++ javascript-beginning-to-mastery-main/part1/31.js
@@ -3,7 +3,9 @@
 // heap memory ["apple", "mango"] 0x11
 
 const fruits = ["apple", "mango"]; // 0x11
-// fruits[0]="kella"
-// fruits=["kella","Banana"]; /**{Not Allowed} */
+// fruits[0]="kella" /** Iss ma hum address ni change kr rahy ussi address ma changings kr rahy hai is lye hum change kr skty "const" ka sath */
+// fruits=["kella","Banana"]; /**{Not Allowed} Iss ma hum address change kr rahy hain iss lye hum "const" ka sath change ni kr skty kch be*/
+
+/**Yeh jo lines likhi hai mene yeh ap relate kr skty hai last topic ma jiss ma btayeh tha apko ka heap memory kia hai address kia hai e.t.c */
 fruits.push("banana");
 console.log(fruits);
